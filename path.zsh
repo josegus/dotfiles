d2f0dd4336e94f3ead13e7313a9a506b026c214f
Gustavo Vasquez <gustavovasquez@MacBook-Pro-de-Gustavo.local>
2022-03-14 02:30:20 -0400

    Add linux and macos binaries path

--- path.zsh
+++ path.zsh
@@ -1,8 +1,17 @@
 # Load system binaries
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:/$HOME/.local/bin
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/snap/bin:$HOME/.local/bin
 
-# Load composer binaries
+# Load homebrew bin
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
+
+# Global bin directories (macos)
+export PATH="/opt/local/bin:/opt/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
+
+# Load composer binaries (linux default)
 export PATH="$HOME/.config/composer/vendor/bin:$PATH"
+
+# Load composer binaries (macos default)
+export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # Load nvm configuration (TODO: Check if this is linux only)
 export NVM_DIR=~/.nvm
