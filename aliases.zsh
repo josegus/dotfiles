0d2d91e55ae218f1bb788ccf9dde389977e1200a
Gustavo Vasquez <jgvv15@gmail.com>
2022-03-20 14:12:25 -0400

    More aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,22 +1,36 @@
+# Shortcuts
+alias c="clear"
+alias db.restart="sudo service mysql restart"
+alias kf.vpn="sudo openvpn --config ~/Documentos/Kickfurther/client.ovpn --auth-user-pass ~/Documentos/Kickfurther/ovpn-pass.txt" # TODO use paths
+alias kf.staging="ssh -i ~/Documentos/Kickfurther/kf.staging ubuntu@172.31.62.89"
+alias reload="source $HOME/.zshrc"
+
+# Directories
+alias sites="cd $HOME/Sites"
+
 # Laravel & PHP
 alias art="php artisan"
+alias cda="composer dumpautoload -o"
 alias tink="php artisan tinker"
 alias mft="php artisan migrate:fresh --env=testing"
 alias sail="vendor/bin/sail"
 
 # PHP unit
 alias test="vendor/bin/phpunit"
-alias t-es="vendor/bin/phpunit -c phpunit.local.xml --exclude slow"
-alias t-os="vendor/bin/phpunit --group slow"
+alias tes="vendor/bin/phpunit -c phpunit.local.xml --exclude slow"
+alias tos="vendor/bin/phpunit --group slow"
 
 # Git
+alias amend="git add . && git commit --amend --no-edit"
+alias com="git add . && git commit ."
+alias commit="git add . && git commit -m"
+alias force="git push --force"
+alias gs="git status"
+alias gb="git branch"
 alias gc="git checkout"
-alias gnb="git checkout -b"
-alias gpo="git pull origin"
+alias gl="git log --oneline --decorate --color"
 alias gms="git checkout staging && git merge -"
-
-# Helpers
-alias dal="composer dumpautoload"
-alias kf.vpn="sudo openvpn --config ~/Documentos/Kickfurther/client.ovpn --auth-user-pass ~/Documentos/Kickfurther/ovpn-pass.txt" # TODO use paths
-alias kf.staging="ssh -i ~/Documentos/Kickfurther/kf.staging ubuntu@172.31.62.89"
-alias db.restart="sudo service mysql restart"
+alias nah="git reset --hard && git clean -df"
+alias pull="git pull"
+alias push="git push"
+alias wip="commit wip"
