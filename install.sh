0f6abc930e211976ae29a73c87d231c6ae54bd16
Gustavo Vasquez <jgvv15@gmail.com>
2022-03-20 19:21:37 -0400

    Installation steps

--- install.sh
+++ install.sh
@@ -1,14 +1,37 @@
+#!/bin/sh
+
+echo "Setting up files & environment... \n";
+
 # Add global gitignore
-rm $HOME/.gitignore_global
-ln -s $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
-git config --global core.excludesfile $HOME/.gitignore_global
+FILE="$HOME/.gitignore_global"
+if test -f "$FILE"; then
+    rm "$FILE"
+fi
+ln -s $HOME/.dotfiles/.gitignore_global "$FILE"
+git config --global core.excludesfile "$FILE"
+echo '- Created .gitignore_global'
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm $HOME/.zshrc
-ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+FILE="$HOME/.zshrc"
+if test -f "$FILE"; then
+    rm "$FILE"
+fi
+ln -s $HOME/.dotfiles/.zshrc "$FILE"
+echo '- Created .zshrc'
 
 # Symlink the Mackup config file to the home directory
-rm $HOME/.mackup.cfg
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
+FILE="$HOME/.mackup.cfg"
+if test -f "$FILE"; then
+    rm "$FILE"
+fi
+ln -s $HOME/.dotfiles/.mackup.cfg "$FILE"
+echo '- Created .mackup.cfg'
 
+echo ''
 echo 'All done!'
+echo ''
+
+echo 'Extra steps:'
+echo '1. Copy path.zsh.{platform}.demo. Replace platform with your OS (linux or mac)'
+echo '2. Copy path_misc.zsh.demo and adjust variables'
+echo '3. Check notion for extra requirements'
